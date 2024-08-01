4a383e94e82a38fd8f4d4d8102a2ce6b46f9e59e
Paul Cera <paul@edusuite.asia>
2024-08-02 01:16:18 +0800

    Added SecurityConfig and new pom dependency

---
+++ src/main/java/com/paulcera/chatbot/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.paulcera.chatbot.config;
+
+import com.paulcera.chatbot.security.filter.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final AuthenticationFilter authenticationFilter;
+
+    @Autowired
+    public SecurityConfig(AuthenticationFilter authenticationFilter) {
+        this.authenticationFilter = authenticationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/v1/**").authenticated()
+                .anyRequest().permitAll()
+            )
+            .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
